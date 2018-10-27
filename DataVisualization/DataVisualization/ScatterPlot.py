e4c529f376e54aceb764fb387ad25fa800aea89c
Nguyễn Đức Lộc <38336618+DucLoc1999@users.noreply.github.com>
2018-10-27 14:13:32 +0700

    fix sb.pairplot

--- DataVisualization/DataVisualization/ScatterPlot.py
+++ DataVisualization/DataVisualization/ScatterPlot.py
@@ -1,8 +1,12 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import matplotlib
+from matplotlib import pyplot as plt
+import seaborn as sb
 from pandas.plotting import scatter_matrix
+
+import matplotlib
+from matplotlib import pyplot as plt
 
 def Scatter_2D(dataFrame, _1st_index, _2nd_index):
     '''
@@ -50,31 +54,30 @@ def Scatter_3D(dataFrame, _1st_index, _2nd_index, _3rd_index):
     ax.set_zlabel(attributeName[_3rd_index])
     plt.show()
 
-def multiMatrix(dataFrame, rowsIndex = np.arange(50), colsIndex_x=None,
-			 colsIndex_y=None, height=2.5, aspect=1, kind='reg', diag_kind='hist'):
+def multiMatrix(dataFrame, rowsIndex, colsIndex_x, colsIndex_y, kind='reg', diag_kind='hist'):
 	'''
-        :param dataFrame: the DataFrame need ploting
-        :param rowsIndex: rows you want to display in your chart:
-			+ pass a num: row to display one row
-            + pass an array: [num1, num2, num3, ...] to display rows you want
-			+ pass: range(start, end), for multiple rows
-        :param colsIndex_x: cols you want to display in your chart by layber x_vars:
+		:param dataFrame: the DataFrame need ploting
+		:param rowsIndex: rows you want to display in your chart:
+			+ pass a num: <row> to display one row
+			+ pass an array: <[num1, num2, num3, ...]> to display rows you want
+			+ pass: <range(start, end)>, for multiple rows
+			+ default = arange(0,50), The first 50 rows from 0 to 49
+		:param colsIndex_x: cols you want to display in your chart by layber x_vars:
 			+ the row of the figure
-        :param colsIndex_y: cols you want to display in your chart by layber x_vars:
+			+ default = all the cols in dataframe
+		:param colsIndex_y: cols you want to display in your chart by layber x_vars:
 			+ the row of the figure
-		:param height: a num which is height of each facet
-		:param aspect: a num, (Aspect * height) gives the width of each facet
+			+ default = all the cols in dataframe
 		:param kind: Kind of plot for the non-identity relationships
 			+ can pass one of {‘scatter’, ‘reg’}, if pass 'reg' it will add a line represent for linear relationship
 		:param diag_kind : Kind of plot for the diagonal subplots
 						+ can pass one of {‘auto’, ‘hist’, ‘kde’}, 'auto' and 'hist' is the same
-        :return: void
-    '''
+		:return: void
+	'''
+
 	head = dataFrame.columns.values
-	if colsIndex_x==None:
-		colsIndex_x = np.arange(len(head))
-	if colsIndex_y==None:
-		colsIndex_y = np.arange(len(head))
-	sns.pairplot(dataFrame.iloc[rowsIndex:], x_vars=head[colsIndex_x],
-				 y_vars=head[colsIndex_y], aspect=aspect, kind=kind, diag_kind=diag_kind)
-	plt.show()
+	sb.set()
+	sb.despine()
+	plt.show(sb.pairplot(dataFrame.iloc[rowsIndex,:],x_vars=head[colsIndex_x], y_vars=head[colsIndex_y],kind=kind,diag_kind=diag_kind))
+
+
